494dfb35d0ea65f9b33e06dccba50aa2bf4ba550
Benjamin Hoefs <bendhoefs@protonmail.com>
2023-11-03 14:08:59 -0500

    Add project files.

---
+++ TestForm.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment4
+{
+    public partial class TestForm : System.Web.UI.Page
+    {
+        string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ben\\Desktop\\Assignment4\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            KarateSchoolDataContext db = new KarateSchoolDataContext(conn);
+            var listItemsRecord = (from x in db.Instructors select new { InstructorId = x.InstructorID, InstructorLastName = x.InstructorLastName }).ToList();
+            testView.DataSource = listItemsRecord;
+            testView.DataBind();
+        }
+    }
+}
